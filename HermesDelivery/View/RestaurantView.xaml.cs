2e893b57f23bd0a88c769cf833bc511b3ac6cce3
KristianSpaqaj <kris49j7@edu.easj.dk>
2020-04-28 13:20:09 +0200

    Start på dokumentet

--- HermesDelivery/View/RestaurantView.xaml.cs
+++ HermesDelivery/View/RestaurantView.xaml.cs
@@ -20,9 +20,9 @@ namespace HermesDelivery.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class RestaurantPageView : Page
+    public sealed partial class RestaurantView : Page
     {
-        public RestaurantPageView()
+        public RestaurantView()
         {
             this.InitializeComponent();
         }
--- HermesDelivery/ViewModel/RestaurantViewModel.cs
+++ HermesDelivery/ViewModel/RestaurantViewModel.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace HermesDelivery.ViewModel
 {
     public class RestaurantViewModel
     {
+        public RestaurantViewModel()
+        {
 
+        }
     }
 }
