5024a4b930ae9bfe427e82aa9200d696854ee7f4
KristianSpaqaj <kris49j7@edu.easj.dk>
2020-05-18 09:53:47 +0200

    Klasser med properties
    Lavede User, Menu og Butik klasser og gav properties til to af dem. Lavede også en UserView og UserViewModel.

--- HermesDelivery/ViewModel/RestaurantViewModel.cs
+++ HermesDelivery/ViewModel/RestaurantViewModel.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 
 namespace HermesDelivery.ViewModel
 {
-    public class RestaurantViewModel
+    public class RestaurantViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public RestaurantViewModel()
         {
 
+        }
+
+        
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
