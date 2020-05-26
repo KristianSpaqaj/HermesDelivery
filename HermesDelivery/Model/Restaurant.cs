e2b8bd9da85e5e4e222cea87118d3f62eefdaf94
KristianSpaqaj <kris49j7@edu.easj.dk>
2020-05-26 11:13:09 +0200

    Revert "Merge branch 'Restaurant' of https://github.com/KristianSpaqaj/HermesDelivery into Restaurant"
    This reverts commit 90bdf0121473448dc474bc8722ccf061e88feaf2, reversing
    changes made to 177cff6e959ced26afca43f8e906f43b6e3e4d85.

--- HermesDelivery/Model/Restaurant.cs
+++ HermesDelivery/Model/Restaurant.cs
@@ -8,9 +8,7 @@ namespace HermesDelivery.Model
 {
     public class Restaurant : Store
     {
-
 		public Restaurant(string name, string address, string number, string open, Menu menu) : base(name, address, number, open, menu)
-
 		{
 
 
