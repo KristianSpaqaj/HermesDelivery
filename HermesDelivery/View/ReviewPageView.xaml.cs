b2a48daefaf537579dc2d1b84a385c4af0b1d9d8
KristianSpaqaj <kris49j7@edu.easj.dk>
2020-06-01 13:28:06 +0200

    Tilføjet ReviewPageView (Kristian & Mikkel via Liveshare)
    Lavede en ReviewPageView hvor man kan skrive en anmeldelse og give en rating.
    Lavede en knap i BuyPageView som navigere til ReviewPageView.

--- HermesDelivery/View/ReviewPageView.xaml.cs
+++ HermesDelivery/View/ReviewPageView.xaml.cs
@@ -20,16 +20,16 @@ namespace HermesDelivery.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BuyPageView : Page
+    public sealed partial class ReviewPageView : Page
     {
-        public BuyPageView()
+        public ReviewPageView()
         {
             this.InitializeComponent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(ReviewPageView));
+            Andmeld.Visibility = Visibility.Visible;
         }
     }
 }
